055b306cc9c6fd623090dc5dc70d743bbf717842
Justin <dog20@live.ca>
2022-06-20 21:44:44 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Justin Kavanaugh on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
